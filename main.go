5cb24ab32839c29de755c6bbc1ed839672fd318d
Elli Nik <131411742+Elli-Nik@users.noreply.github.com>
2024-01-12 18:17:57 +0300

    Update main.go

--- main.go
+++ main.go
@@ -18,25 +18,4 @@ func main() {
 	}
 	FullHouseDb()
 }
-
-//defer conn.Close(context.Background())
-//
-//nameInsert := "Liza"
-////var name string
-////var id string
-//tx, err := conn.Begin(context.Background())
-//if err != nil {
-//  fmt.Println(err)
-//}
-//defer tx.Rollback(context.Background())
-//tx.Exec(context.Background(), "insert into family (name) values ($1)", nameInsert)
-//tx.Commit(context.Background())
-//rows, err := conn.Query(context.Background(), "select * from family")
-//if err != nil {
-//  fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-//  os.Exit(1)
-//}
-//for rows.Next() {
-//  rows.Scan(&name, &id)
-//  fmt.Println("name - ", name, "id - ", id)
-//}
+}
